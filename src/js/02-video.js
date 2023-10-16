5b8ad6cdde43e8c556cf5fbc5db0e3031714cc2e
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-10-16 21:47:57 +0300

    five

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo(document.getElementById('vimeo-player'), {
+  id: 236203659,
+  width: 640,
+});
+const VCT_KEY = 'videoplayer-current-time';
+const onPlay = (evt) => {
+  localStorage.setItem(VCT_KEY, JSON.stringify(evt.seconds));
+  console.log(evt.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+const savedTime = JSON.parse(localStorage.getItem(VCT_KEY)) || 0;
+player.setCurrentTime(savedTime);
